c950fe4bf72a0e82fc2869b273b66fa897bc3c65
Peter Liddle <development.peter@liddle.network>
2024-06-27 12:10:56 -0500

    Initial Commit

---
+++ DemoTaskList/DemoTaskListApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoTaskListApp.swift
+//  DemoTaskList
+//
+//  Created by Peter Liddle on 6/27/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoTaskListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
